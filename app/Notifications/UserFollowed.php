3bf053d382e9ad54863a23caa04dbec07ddbca8c
Rene Ismael <Ismael_br7@hotmail.com>
2018-04-09 03:46:54 +0000

    add archives finals and do deploy in heroku

---
+++ app/Notifications/UserFollowed.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Notifications;
+
+use App\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\BroadcastMessage;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class UserFollowed extends Notification
+{
+    use Queueable;
+
+    public $user;
+    public $follower;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(User $follower)
+    {
+        $this->follower = $follower; 
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail', 'database', 'broadcast'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->subject('You have a new follower')
+                    ->greeting('Hello '. $notifiable->name)
+                    ->line('The user @' . $this->follower->username . 'have followed you')
+                    ->action('See Profile', url('/' . $this->follower->username))
+                    //->line('Thank you for using our application!');
+                    ->salutation('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            'follower' => $this->follower,
+        ];
+    }
+
+    public function toBroadcast($notifiable)
+    {
+        return new BroadcastMessage([
+
+            'data' => $this->toArray($notifiable),
+
+        ]);
+    }
+}
